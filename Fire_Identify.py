68c0f24be2dd3268ac0fd381e858a0eaefe336ba
flyjet6 <147256426+flyjet6@users.noreply.github.com>
2023-10-07 13:33:15 -0400

    Update Fire_Identify.py

--- Fire_Identify.py
+++ Fire_Identify.py
@@ -10,24 +10,20 @@ for image_path in x:
     image = Image.open(image_path)
     image = image.resize(target_size)
     resized_images.append(np.array(image))
-
-# Define the Sequential model
+y = []
 model = tf.keras.Sequential()
 
-# Convolutional layers for feature extraction
 model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
 model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
 
-# Flatten the feature maps
 model.add(tf.keras.layers.Flatten())
 
-# Fully connected layers
 model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='sigmoid'))  # Output layer with sigmoid activation for binary classification
+model.add(tf.keras.layers.Dense(1, activation='sigmoid'))  # Output layer with sigmoid activation for binary classification
 
-# Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit()
+model.fit(resized_images, y)
+model.save("Model_Fires.keras")
 model.summary()
